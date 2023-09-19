dbc79b5d9bdfcf429d9e4fb0aca2217eb18930b8
unknown <liuxiaolun@hikvision.com>
2023-09-19 17:30:58 +0800

    doc

--- src/render/CenterBox/ChessBoard.jsx
+++ src/render/CenterBox/ChessBoard.jsx
@@ -3,12 +3,109 @@ import "./ChessBoard.css"
 import Board from "./ChessBoard/Board"
 
 export default function () {
-    let minimize = () => {
+    window.addEventListener("resize",(e)=>{
+        let w = document.body.clientWidth - 60;
+        let h = document.body.clientHeight - 32 - 28 - 60;
+        if(h/9*8 > w){
+            if(w/8*9 > h){
+                
+            } 
+        }
 
-    }
+    })
     return (
         <div class="chessBoard">
-            <Board id={"boardImg"}></Board>
+            <div class="chessBoardContainer">
+                <div class="chessBoardRow">
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                </div>
+                <div class="chessBoardRow">
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                </div>
+                <div class="chessBoardRow">
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                </div>
+                <div class="chessBoardRow">
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                </div>
+                <div class="chessBoardRow">
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                </div>
+                <div class="chessBoardRow">
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                </div>
+                <div class="chessBoardRow">
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                </div>
+                <div class="chessBoardRow">
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                    <div class="chessBoardBox"></div>
+                </div>
+            </div>
+            {/* <Board id={"boardImg"}></Board> */}
             {/* <img id="boardImg" src="./res/board.svg" alt="" /> */}
         </div>
     )
